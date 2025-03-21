3babb8d120dacb869b8e53b9e48ea4b2f76652b6
Rajdwip-69 <143189655+Rajdwip-69@users.noreply.github.com>
2025-03-21 12:58:48 +0530

    Add files via upload

--- basic.java
+++ basic.java
@@ -1,23 +1,22 @@
-package Arrays;
+/******************************************************************************
 
-public class basic {
-    public static void main(String[] args) {
-        int arr [] = new int[5];
-        arr[0] = 10;
-        arr[1] = 20;
-        arr[2] = 30;
-        arr[3] = 40;
-        arr[4] = 50;
-//        System.out.println(arr[0]);
-        System.out.println(arr[1]);
-        System.out.println(arr[2]);
-        System.out.println(arr[3]);
-        System.out.println(arr[4]);
-        arr[0] =99;
-        System.out.println(arr[0]);
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
 
-
-
-
+*******************************************************************************/
+class basic{
+    void onedimentionalarray(){
+        int [] arr = {1,2,3,4,5};
+        for(int i =0 ; i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+    
+    public static void main (String [] args){
+        basic b1 = new basic();
+        b1.onedimentionalarray();
+        
     }
 }
